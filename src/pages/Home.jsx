0598dca44d7115a99095849d2c8d942e078aeda3
N3-Ricky <165741930+NRicky25@users.noreply.github.com>
2025-08-08 19:02:55 +1000

    Frontend setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-800">
+      <h1 className="text-3xl text-white">HelloWorld!</h1>
+    </div>
+  );
+};
+
+export default Home;
